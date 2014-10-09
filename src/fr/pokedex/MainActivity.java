4d12b511d720b086e93b1997047890df52b01ed6
delta.danger <delta.danger@b6a16d1b-dfe3-12ff-5e36-5033d6c796d0>
2014-10-09 04:27:14 +0000

    Fixed type images for English

--- src/fr/pokedex/MainActivity.java
+++ src/fr/pokedex/MainActivity.java
@@ -10,16 +10,20 @@ import fr.pokedex.data.DataHolder;
  * TODO:
  * / Ajouter lien entre Ptéra et Mega-Ptéra
  * / Ajouter images pour les formes alternatives
- * / Evolutions disparues 
+ * / Evolutions disparues
+ * / Clavier qui apparait quand on ferme les details d'un talent / change de langue
+ * / Corriger les images de types anglais
+ * - Trier les types par ordre alphabétique dans la langue sélectionnée
+ * ? Layout de l'affichage des détails (certains mots sortent tel: Y530 (hwY530-U00))
+ * - Erreurs diverses
+ * - Espagol
+ * - Portugais
+ * - Allemand
  * 
- * - Clavier qui apparait quand on ferme les details d'un talent / change de langue
- * - Layout de l'affichage des détails (certains mots sortent tel: Y530 (hwY530-U00))
- * - Trier les types par ordre alphabétiques dans la langue sélectionnée
  * - Recherche par texte pur (incluant par numéro)
- * X Recherche par type
  * - Theme sombre
  * - Liste des attaques apprises
- * - Erreurs diverses
+ * X Recherche par type
  */
 
 public class MainActivity extends Activity {
